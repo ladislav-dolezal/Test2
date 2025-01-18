87b3e21e0d32e6ff6e2254c27f8b47f3f5d07980
lado-dev <ladislav.dolezal@volny.cz>
2025-01-18 16:12:57 +0100

    add edo

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 
-data = {'Name': ['John', 'Anna', 'Peter', 'Linda', 'Pepo', 'lado'],
-    'Age': [28, 24, 35, 32, 45, 50]}
+data = {'Name': ['John', 'Anna', 'Peter', 'Linda', 'Pepo', 'lado', 'edo'],
+    'Age': [28, 24, 35, 32, 45, 50, 47]}
 
 df = pd.DataFrame(data)
 total_age = df['Age'].sum()
